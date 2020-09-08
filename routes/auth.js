f42cb41b0cfd402e3e39bd9b9d9a26637b49c3e1
Rabra <rabira.hierpa@gmail.com>
2020-09-08 16:31:32 +0300

    Completed the Register post method

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,35 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-// VALIDATION
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-	name: Joi.string().min(4).required(),
-	email: Joi.string().min(6).required().email(),
-	password: Joi.string().min(6).required(),
-});
+const bcrypt = require('bcryptjs');
+const { registerValidation } = require('../validation');
+const { loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
 	// validate data
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
+	// Check if the user is already in the database
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist)
+		return res.status(400).send('Email is already registred with a user');
+
+	// Hash passwords
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	// Create a new user
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashedPassword,
+	});
 	try {
-		const value = await schema.validate(req.body);
-		// console.log('Errro Message' + value.error.details[0].message);
-		res.send(value);
-	} catch (error) {
-		console.error(error);
+		const savedUser = await user.save();
+		res.send({ user: user._id });
+	} catch (err) {
+		res.status(400).send(err);
 	}
-
-	// const user = new User({
-	// 	name: req.body.name,
-	// 	email: req.body.email,
-	// 	password: req.body.password,
-	// });
-	// try {
-	// 	// console.log(req);
-	// 	const savedUser = await user.save();
-	// 	res.send(savedUser);
-	// } catch (err) {
-	// 	res.status(400).send(err);
-	// }
 });
 
 // router.post('/login')
